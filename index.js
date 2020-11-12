79b449737c4bd46a87bb880fada133128c73f70e
jeehoo0767 <jeehoo0767@naver.com>
2020-11-12 21:08:41 +0900

    Connection Mongodb Safety

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended : true }));
+//application/json
+app.use(bodyParser.json());   
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://jihupark:jihupark1234@cloneyoutube.j6ofu.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
         useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex : true, useFindAndModify : false
     }).then(()=>{
         console.log('mongodb connected');
@@ -12,6 +22,17 @@ mongoose.connect('mongodb+srv://jihupark:jihupark1234@cloneyoutube.j6ofu.mongodb
 
 app.get('/', (req, res) => {
     res.send('hello world hi ~~~~');
+});
+
+app.post('/register', (req, res) => {
+    // 회원 가입 정보를 client에서 요청 받으면 
+    // 그 정보들을 데이터 베이스에 insert
+    const user = new User(req.body); // user 인스턴스 생성
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success : false, err });
+        else return res.status(200).json({success : true});
+    });
 });
 
 app.listen(port, () => {
