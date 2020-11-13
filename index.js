55850349f3b8ca0554ed28ee3f1bd2eb3ad623ad
alsruddl0805 <66991772+jeehoo0767@users.noreply.github.com>
2020-11-13 17:44:26 +0900

    add webtoken module

--- index.js
+++ index.js
@@ -34,6 +34,31 @@ app.post('/register', (req, res) => {
     });
 });
 
+app.post('/login', (req, res) => {
+    // 요청된 이메일 데이터베이스 에서 찾음
+    //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인
+    //비밀번호 까지 맞다면 토큰 생성
+
+    User.findOne({email : req.body.email}, (err, user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess : false,
+                message : " 이메일에 해당하는 유저가 없음."
+            })
+        }
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch){
+                return res.json({ loginSuccess : false, message : "비밀번호가 틀렸습니다."})
+            };
+            user.generateToken((err, user) => {
+                
+            })
+
+        })
+    })
+})
+
 app.listen(port, () => {
     console.log('5000번 포트 성공');
 })
--- models/User.js
+++ models/User.js
@@ -46,9 +46,17 @@ userSchema.pre('save', function(next){
                 next();
             });
         });
-    };
+    }else {
+        next();
+    }
 });
 
+userSchema.method.comparePassword = function(plainPassword, cb){
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err),
+        cb(null, isMatch)
+    })
+}
 
 const User = mongoose.model('User', userSchema);
 
