905e62a3af257835bd2efde348d22061d12a0f45
jeehoo0767 <jeehoo0767@naver.com>
2020-11-20 22:43:18 +0900

    setUpProxy version issue clear

--- client/src/setUpProxy.js
+++ client/src/setUpProxy.js
@@ -1,6 +1,22 @@
-const proxy = require("http-proxy-middleware");
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+
 
 module.exports = function(app) {
-  app.use(proxy("/api/hello", { target: "http://localhost:5000" }));
-  app.use(proxy("/api/user", { target: "http://localhost:5000" }));
+
+app.use(
+
+'/api',
+
+createProxyMiddleware({
+
+target: 'http://localhost:5000',
+
+changeOrigin: true,
+
+})
+
+);
+
 };
