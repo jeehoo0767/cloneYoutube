66cfd7a3f2d2c65714196e352f395e2fe84e13ac
jeehoo0767 <jeehoo0767@naver.com>
2020-11-24 02:28:08 +0900

    Create RegisterPage

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 //로그인 페이지 action 을 reducer에 넘겨줌
 import {
-    LOGIN_USER
+    LOGIN_USER, REGISTER_USER
 } from './types';
 
 export function loginUser(dataTosubmit) {
@@ -10,6 +10,16 @@ export function loginUser(dataTosubmit) {
     
         return {
             type : "LOGIN_USER",
+            payload: request
+        }
+}
+
+export function registerUser(dataTosubmit) {
+    const request = Axios.post('/api/users/register', dataTosubmit)
+        .then(response => response.data)
+    
+        return {
+            type : "REGISTER_USER",
             payload: request
         }
 }
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,10 +1,80 @@
-import React from 'react'
 
-function RegisterPage() {
+import React,{ useState } from 'react'
+import { Provider, useDispatch } from 'react-redux';
+import { registerUser } from '../../../_actions/user_action';
+
+
+function RegisterPage(props) {
+
+    const dispatch = useDispatch();
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+    const [Name, setName] = useState("")
+    const [ConfirmPassword, setConfirmPassword] = useState("")
+
+
+    const onEmailHandler = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const onPasswordHandler = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const onNameHandler = (e) => {
+        setName(e.target.value);
+    }
+
+    const onConfirmPasswordHandler = (e) => {
+        setConfirmPassword(e.target.value);
+    }
+
+    const onSubmitHendler = (e) => {
+        e.preventDefault();
+
+        if(Password !== ConfirmPassword) {
+            return alert('비밀번호를 다시 확인 해주세요.')
+        }
+
+        let body = {
+            email : Email,
+            password : Password,
+            name : Name
+        };
+
+        dispatch(registerUser(body))
+        .then(response => {
+            if(response.payload.success) {
+                props.history.push("/login");
+            } else {
+                alert("회원가입 실패");
+            }
+        })
+
+    }
     return (
-        <div>
-            RegisterPage
-        </div>
+        <div style={{display:'flex', justifyContent : 'center', alignItems : 'center',
+        width : '100%', height : '100vh'
+    }}>
+        <form style={{ display : 'flex', flexDirection : 'column'}}
+            onSubmit = { onSubmitHendler }
+        >
+            <label htmlFor="">Email</label>
+            <input type="email" value={Email} onChange={ onEmailHandler }/>
+
+            <label htmlFor="">Name</label>
+            <input type="text" value={Name} onChange={ onNameHandler }/>
+
+            <label htmlFor="">Password</label>
+            <input type="password" value={Password} onChange = { onPasswordHandler }/>
+            
+            <label htmlFor="">Confirm Password</label>
+            <input type="password" value={ConfirmPassword} onChange = { onConfirmPasswordHandler }/>
+            <br/>
+            <button>Join</button>
+        </form>
+    </div>
     )
 }
 
