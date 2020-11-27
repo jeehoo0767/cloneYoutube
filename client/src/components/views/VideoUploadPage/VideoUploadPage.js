226fec7a7f0a42a2330faabfbc4f728fef03eb49
jeehoo0767 <jeehoo0767@naver.com>
2020-11-27 21:51:32 +0900

    file save server

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -54,7 +54,7 @@ function VideoUploadPage() {
         Axios.post('/api/video/uploadfiles', formData, config)
         .then(response => {
             if(response.data.success) {
-
+                console.log(response.data);
             } else {
                 alert('비디오 업로드 실패')
             }
